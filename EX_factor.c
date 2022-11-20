ff515ff50def1c96f3c136ec0e15a8bba8d4f3ed
benjamin770414 <117279879+benjamin770414@users.noreply.github.com>
2022-11-20 17:16:17 +0800

    Update EX_factor.c

--- EX_factor.c
+++ EX_factor.c
@@ -5,19 +5,18 @@
 #include <stdio.h>
 
 int main()
-{
-    int n = 60;
+{    
+    int n = 1000;
     /*
     printf("請輸入一個正整數: ");
     scanf("%d", &n);
-    */    
-    printf(" %d 的因數有: ", n);
+    */
+    printf("Factors of %n are: ", n);
     int i;
     for(i=1; i<=n; i++){
         if(n % i == 0){
             printf(" %d ", i);
         }
     }
-
     return 0;
 }
