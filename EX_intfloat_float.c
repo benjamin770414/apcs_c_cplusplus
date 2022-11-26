e2191e50295d13b041b0065ad2ff3cf5cb9f840b
benjamin770414 <117279879+benjamin770414@users.noreply.github.com>
2022-11-26 15:49:29 +0800

    Create EX_intfloat_float.c

---
+++ EX_intfloat_float.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    int i,n=60;
+    // long pow2=1;
+    long pow2= 1152921504606846976;
+    for(i=0;i<n;i++){
+        // pow2 =  pow2 * 2;
+        pow2 =  pow2 / 2;
+    }
+    printf("2 pow %d = %ld \n",n,pow2);
+    return 0;
+}
